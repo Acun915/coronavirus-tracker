e1904367c67690a43337e5b277ece97a12d7fe0b
Przemyslaw <przemoacun@gmail.com>
2020-12-10 16:00:39 +0100

    HomeController: added model.attribute with total reported cases, home.html: added jumbotron to display total reported cases

--- coronavirus-tracker/src/main/java/pl/pm/coronavirustracker/controllers/HomeController.java
+++ coronavirus-tracker/src/main/java/pl/pm/coronavirustracker/controllers/HomeController.java
@@ -4,7 +4,10 @@
     import org.springframework.stereotype.Controller;
     import org.springframework.ui.Model;
     import org.springframework.web.bind.annotation.GetMapping;
+    import pl.pm.coronavirustracker.models.LocationStats;
     import pl.pm.coronavirustracker.services.CoronaVirusDataService;
+
+    import java.util.List;
 
     @Controller
     public class HomeController {
@@ -18,7 +21,11 @@
 
         @GetMapping("/")
         public String home(Model model) {
-            model.addAttribute("locationStats",coronaVirusDataService.getAllStats());
+            List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+            model.addAttribute("locationStats",allStats);
+            model.addAttribute("totalReportedCases", allStats.stream()
+                    .mapToInt(locationStat -> locationStat.getLatestTotalCases())
+                    .sum());
             return "home";
         }
 
