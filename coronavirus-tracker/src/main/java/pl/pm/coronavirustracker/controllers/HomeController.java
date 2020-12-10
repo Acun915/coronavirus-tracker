4e787b1131a2d7e1a1e2f1bf573117d2e088efbe
Przemyslaw <przemoacun@gmail.com>
2020-12-10 15:38:44 +0100

    CoronaVirusDataController @RestController -> @Controller, CoronaVirusDataService getAllStats() added, LocationStats getState() package private -> public, home.html and HomeController added to display data on the web

---
+++ coronavirus-tracker/src/main/java/pl/pm/coronavirustracker/controllers/HomeController.java
@@ -0,0 +1,25 @@
+    package pl.pm.coronavirustracker.controllers;
+
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.stereotype.Controller;
+    import org.springframework.ui.Model;
+    import org.springframework.web.bind.annotation.GetMapping;
+    import pl.pm.coronavirustracker.services.CoronaVirusDataService;
+
+    @Controller
+    public class HomeController {
+
+        private final CoronaVirusDataService coronaVirusDataService;
+
+        @Autowired
+        public HomeController(CoronaVirusDataService coronaVirusDataService) {
+            this.coronaVirusDataService = coronaVirusDataService;
+        }
+
+        @GetMapping("/")
+        public String home(Model model) {
+            model.addAttribute("locationStats",coronaVirusDataService.getAllStats());
+            return "home";
+        }
+
+    }
